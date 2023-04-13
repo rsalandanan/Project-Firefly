52726ab13d147faa8f43f1d19c648ce21904f48a
duwpo <raphaelyapsalandanan@gmail.com>
2023-04-13 23:02:50 +0800

    tmp import and distance

--- Project-Firefly/Assets/Scripts/GameManager.cs
+++ Project-Firefly/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,15 +8,20 @@ public class GameManager : MonoBehaviour
     private float _timer;
     public float timeBeforeSpawn;
     public GameObject projectilePrefab;
+    public TextMeshProUGUI distanceUI;
+    private float _distance;
 
     private void Update()
     {
         _timer += Time.deltaTime;
+        _distance += Time.deltaTime;
         if (_timer >= timeBeforeSpawn)
         {
             _timer = 0;
             int ranNum = Random.Range(0, 3);
             Instantiate(projectilePrefab, spawnPoints[ranNum].transform.position, projectilePrefab.transform.rotation);
         }
+
+        distanceUI.text = "Distance: " + _distance.ToString("F2");
     }
 }
