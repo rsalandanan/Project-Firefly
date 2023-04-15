7ad150c681e1c23e95abc0698214d0cd4a8c1b2b
duwpo <raphaelyapsalandanan@gmail.com>
2023-04-15 12:13:41 +0800

    Player getting hit animation

--- Project-Firefly/Assets/Scripts/PlayerScript.cs
+++ Project-Firefly/Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,30 @@
-
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
    public float jumpForce;
    private Rigidbody2D _rigidbody2D;
    private BoxCollider2D _boxCollider2D;
    [SerializeField] private LayerMask canJump;
+   
    [SerializeField] private LayerMask enemies;
    private float _timer;
    public float shootTime;
    private bool _isAttacking;
    public GameObject attackPoint;
    public float attackRadius;
+
+   private int _hpPoint = 3;
+   public TextMeshProUGUI hpUI;
+   private bool _gotHit;
    
 
    private Animator _animator;
    private static readonly int State = Animator.StringToHash("state");
-   private enum CharacterState {Running,Jumping, Falling, Attacking}
+   private enum CharacterState {Running,Jumping, Falling, Attacking,GettingHit}
 
    public GameManager gameManager;
    
@@ -28,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
       _rigidbody2D = GetComponent<Rigidbody2D>();
       _boxCollider2D = GetComponent<BoxCollider2D>();
       _animator = GetComponent<Animator>();
+      hpUI.text = "HEALTH: " + _hpPoint;
    }
 
    private void Update()
@@ -47,7 +55,6 @@ public class PlayerMovement : MonoBehaviour
 
    private void Attack()
    {
-      
       _timer += Time.deltaTime;
       if (Input.GetKey(KeyCode.Mouse0) &&  _timer >= shootTime)
       {
@@ -66,6 +73,18 @@ public class PlayerMovement : MonoBehaviour
       }
    }
 
+   private IEnumerator OnTriggerEnter2D(Collider2D col)
+   {
+      if (col.gameObject.CompareTag("EnemyProjectile"))
+      {
+         _hpPoint--;
+         hpUI.text = "HP: " + _hpPoint;
+         _gotHit = true;
+         yield return new WaitForSeconds(0.1f);
+         _gotHit = false;
+      }
+   }
+
    private void OnDrawGizmos()
    {
       Gizmos.DrawWireSphere(attackPoint.transform.position,attackRadius);
@@ -76,7 +95,6 @@ public class PlayerMovement : MonoBehaviour
       var bounds = _boxCollider2D.bounds;
       return Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, 0.1f, canJump);
    }
-
    private void CharacterAnimation()
    {
       CharacterState state;
@@ -91,6 +109,10 @@ public class PlayerMovement : MonoBehaviour
       else if (_isAttacking)
       {
          state = CharacterState.Attacking;
+      }
+      else if (_gotHit)
+      {
+         state = CharacterState.GettingHit;
       }
       else
       {
