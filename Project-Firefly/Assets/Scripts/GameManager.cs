5fb3cb7212972af47a4b4aaeca5f4378798b7503
duwpo <raphaelyapsalandanan@gmail.com>
2023-04-17 00:37:37 +0800

    2 enemy projectiles / random selection of projectiles

--- Project-Firefly/Assets/Scripts/GameManager.cs
+++ Project-Firefly/Assets/Scripts/GameManager.cs
@@ -5,9 +5,10 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] spawnPoints;
+    public GameObject[] projectiles;
     private float _timer;
     public float timeBeforeSpawn;
-    public GameObject projectilePrefab;
+    //public GameObject projectilePrefab;
     public TextMeshProUGUI distanceUI;
     public TextMeshProUGUI scoreUI;
     public int killCount;
@@ -22,7 +23,8 @@ public class GameManager : MonoBehaviour
         {
             _timer = 0;
             int ranNum = Random.Range(0, 3);
-            Instantiate(projectilePrefab, spawnPoints[ranNum].transform.position, projectilePrefab.transform.rotation);
+            int ranNumb = Random.Range(0, 2);
+            Instantiate(projectiles[ranNumb], spawnPoints[ranNum].transform.position, projectiles[ranNumb].transform.rotation);
         }
 
         distanceUI.text = "Distance: " + _distance.ToString("F2");
